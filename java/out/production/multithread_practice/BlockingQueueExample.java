1ce24c3641ddf3329f3789db8a2749d0f2f1dcf9
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2025-04-14 18:54:03 -0400

    blocking q

--- java/out/production/multithread_practice/BlockingQueueExample.java
+++ java/out/production/multithread_practice/BlockingQueueExample.java
@@ -4,32 +4,56 @@ package out.production.multithread_practice;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class BlockingQueueExample {
     public static void main(String[] args) throws InterruptedException {
         BlockingQueue<Integer> bq = new LinkedBlockingQueue<>(3);
-        tempSensorProd(bq);
-        tempSensorCons(bq);
+        Thread t1 = new Thread(() -> tempSensorProd(bq));
+        Thread t2 = new Thread(() -> tempSensorCons(bq));
+
+        t1.start();
+        t2.start();
 
         Thread.sleep(5000);
+
+        t1.interrupt();
+        t2.interrupt();
+
+        t1.join();
+        t2.join();
+        System.out.println("Main thread finished");
     }
 
     private static void tempSensorProd(BlockingQueue<Integer> bq) {
         try {
-            while(true) {
-                Random r = new Random();
-                int val = r.nextInt();
-                System.out.println("Temp sensor value generated %d" + val);
+            Random r = new Random();
+            while(!Thread.currentThread().isInterrupted()) {
+                int val = r.nextInt(100);
+                System.out.println("Temp sensor value generated " + val);
                 bq.put(val);
+                TimeUnit.MILLISECONDS.sleep(new Random().nextInt(500) + 500);
             }
+            System.out.println("Producer finished");
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Producer interrupted");
+            Thread.currentThread().interrupt();
         }
     }
 
     private static void tempSensorCons(BlockingQueue<Integer> bq) {
-        while(true) {
-            System.out.println("Consumer received the value" + bq.poll());
+        try {
+            while(!Thread.currentThread().isInterrupted()) {
+                Integer val = bq.take();
+                if(val != null) {
+                    System.out.println("Consumer received the value " + val);
+                    TimeUnit.MILLISECONDS.sleep(new Random().nextInt(800) + 200);
+                }
+            }
+            System.out.println("Consumer finished");
+        } catch (InterruptedException e) {
+            System.out.println("Consumer interrupted");
+            Thread.currentThread().interrupt();
         }
     }
 }
